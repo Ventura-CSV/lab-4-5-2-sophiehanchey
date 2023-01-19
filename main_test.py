1bba003969a668fdb1f6464c58706e1d5185ac06
Kyu Lee <kwooler@gmail.com>
2023-01-18 22:02:52 -0800

    L45-2

--- main_test.py
+++ main_test.py
@@ -7,38 +7,22 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = 'A\nB\n30'
-    sys.stdin = io.StringIO(datastr)
+    # datastr = 'A\nB\n30'
+    # sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
+    rdvals = list(map(int, lines[0].split()))
+    print(rdvals)
     print(lines)
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*30[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
-
-
-def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = 'Python\nProgram\n100'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*100[\w,\W]*'
-    res = re.search(regex_string, lines[0])
+    tot = sum(rdvals)
+    print('total ', tot)
+    regex_string = r'[\w,\W]*' + str(tot) + r'[\w,\W]*'
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
