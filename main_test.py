f8d0448c9f95a00d417eeabe9dbe1de4abfbe079
Kyuwoong Lee <kwooler@gmail.com>
2023-01-18 21:59:32 -0800

    Initial commit

---
+++ main_test.py
@@ -0,0 +1,44 @@
+import main
+import io
+import sys
+import re
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = 'A\nB\n30'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    regex_string = r'[\w,\W]*30[\w,\W]*'
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = 'Python\nProgram\n100'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    regex_string = r'[\w,\W]*100[\w,\W]*'
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
